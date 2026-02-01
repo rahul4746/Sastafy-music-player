72f143a0a2e0f300402d1ec6f96fea15c04c131f
Rahul Naskar <142179532+rahul4746@users.noreply.github.com>
2026-02-01 17:00:28 +0530

    Upgrade service worker cache version to v1.5

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Service Worker
    =============================== */
 
-const CACHE_NAME = "sastafy-v1.4";
+const CACHE_NAME = "sastafy-v1.5";
 
 const FILES_TO_CACHE = [
   "./",                // root
@@ -70,6 +70,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
