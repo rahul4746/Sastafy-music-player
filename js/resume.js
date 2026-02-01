367cd7dadfc32525f2c0fdd9bb5a8bc2886c3c55
Rahul Naskar <142179532+rahul4746@users.noreply.github.com>
2026-02-01 20:22:44 +0530

    Add files via upload

---
+++ js/resume.js
@@ -0,0 +1,23 @@
+const RESUME_KEY = "sastafy-resume";
+
+/* Save current song + time */
+function saveResumeState(index, time) {
+  localStorage.setItem(
+    RESUME_KEY,
+    JSON.stringify({ index, time })
+  );
+}
+
+/* Load resume state */
+function loadResumeState() {
+  try {
+    return JSON.parse(localStorage.getItem(RESUME_KEY));
+  } catch {
+    return null;
+  }
+}
+
+/* Clear resume state (optional) */
+function clearResumeState() {
+  localStorage.removeItem(RESUME_KEY);
+}
